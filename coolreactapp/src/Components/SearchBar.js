b4ff3e9ef31a3c2a221a2288fa06fc65888948c2
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-13 23:08:46 +0400

    The project has ended.

--- coolreactapp/src/Components/SearchBar.js
+++ coolreactapp/src/Components/SearchBar.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import './SearchBar.css';
-import SearchIcon from '@mui/icons-material/Search';
+import React, { useState } from "react";
+import "./SearchBar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from '@mui/icons-material/Close';
 
-const SearchBar = ({placeholder, data}) => {
+const SearchBar = ({ placeholder, data }) => {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState('');
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+  const clearInput = () => {
+    setWordEntered('')
+    setFilteredData([]);
+  }
+
   return (
-    <div className='search'>
-        <div className='searchInputs'>
-            <input type='text' placeholder={placeholder}/>
-            <div className='searchIcon'><SearchIcon/></div>
-
+    <div className="search">
+      <div className="searchInputs">
+        <input type="text" value={wordEntered} placeholder={placeholder} onChange={handleFilter} />
+        <div className="searchIcon">
+          {
+           filteredData.length === 0  ? <SearchIcon /> : <CloseIcon id='clearBtn' onClick={clearInput}/> 
+          }
+        
         </div>
-        <div className='dataResult'>
-          {data.map((value, index)=>{
-            return <a key={index} className='dataItem'  href={value.link} target='_blank'>
-              <p>{value.title}</p>
-            </a>
+      </div>
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.slice(0, 15).map((value, index) => {
+            return (
+              <a
+                key={index}
+                target="_blank"
+                className="dataItem"
+                href={value.link}
+              >
+                <p>{value.title}</p>
+              </a>
+            );
           })}
         </div>
-
+      )}
     </div>
   );
-}
+};
 
 export default SearchBar;
